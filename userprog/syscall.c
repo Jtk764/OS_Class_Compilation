51a03fa4dc19d695dfd777b2f3f5fab1b644b5b0
Ram Muthukumar <ram_muthukumar@utexas.edu>
2017-10-26 17:57:47 -0500

    got through until exec tests

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -16,7 +16,8 @@ struct fdesc* findFD(struct list *l, int fd){
     struct list_elem *e;
     for (e = list_begin(l); e != list_end(l); e = list_next(e))
     {
-        if(list_entry(e, struct fdesc, elem)->fd == fd){
+        int temp = list_entry(e, struct fdesc, elem)->fd;
+        if(temp == fd){
             return list_entry(e, struct fdesc, elem);
         }
     }
@@ -35,7 +36,7 @@ get_user (const uint8_t *uaddr)
   asm ("movl $1f, %0; movzbl %1, %0; 1:"
        : "=&a" (result) : "m" (*uaddr));
   return result;
-}
+} 
  
 /* Writes BYTE to user address UDST.
    UDST must be below PHYS_BASE.
@@ -50,16 +51,17 @@ put_user (uint8_t *udst, uint8_t byte)
 }
 
 static bool
-check_string(uint8_t *start){
+check_string(char *start){
     char temp;
     int i = 0;
     if(start > PHYS_BASE) return false;
-    while(temp = get_user(start + i)){
+    temp = get_user(start + i);
+    while(temp != -1 && temp != NULL){
         i++;
         if((start + i)> PHYS_BASE) return false;
+        temp = get_user(start+i);
     }
-    if(temp = NULL) return true;
-    if(temp = -1) return false;
+    if(temp == NULL) return true;
     return false;
 }
 
@@ -103,7 +105,9 @@ syscall_handler (struct intr_frame *f UNUSED)
     char codeEnd[] = ")";
     int length;
     int test=-1;
+    int ftemp;
     char* name = thread_name();
+    struct file *ret;
     if ( f->esp > PHYS_BASE ){  
         printf("%s: exit(%d)\n", name, test);
         thread_exit(); 
@@ -152,12 +156,13 @@ syscall_handler (struct intr_frame *f UNUSED)
             break;
         case SYS_EXEC:
             //check the command line string for seg faults
-            if(!check_string((uint8_t *)(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string((uint8_t *)*(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
 
             //process execute should handle the token parsing for the argument to exec            
             tid_t thd;
             sema_down(&sema);
-            if((thd = process_execute(*(call+1))) == TID_ERROR)
+            thd = process_execute(*(call+1));
+            if(thd == TID_ERROR)
                 (f->eax) = -1;
             else{
                 for(e = list_begin(&current->children); e != list_end(&current->children); e = list_next(e)){
@@ -171,23 +176,23 @@ syscall_handler (struct intr_frame *f UNUSED)
             sema_up(&sema);
             break;
         case SYS_CREATE:
-            if(!check_string((uint8_t *)(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string((char*)*(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
             sema_down(&sema);
             (f->eax) = filesys_create(*(call+1), *(call+2));
             sema_up(&sema);
             break;
         case SYS_REMOVE:
-            if(!check_string((uint8_t *)(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string((uint8_t *)*(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
             sema_down(&sema);
             (f->eax) = filesys_remove(*(call+1));
             sema_up(&sema);
             break;
         case SYS_OPEN:
-            if(!check_string((uint8_t *)(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string((uint8_t *)*(call + 1))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
             sema_down(&sema);
-            struct file *ret;
-            if((ret = filesys_open(*(call+1)) == NULL)) (f->eax) = -1;
-                        else{
+            ret = filesys_open(*(call+1));
+            if(ret == NULL) (f->eax) = -1;
+            else{
                 struct fdesc *filed = (struct fdesc*)malloc(sizeof(struct fdesc));
                 filed->f = ret;
                 struct thread *current = thread_current();
@@ -202,16 +207,20 @@ syscall_handler (struct intr_frame *f UNUSED)
                 else{
                     for(e=list_begin(&(current->fdt)); e != list_end(&(current->fdt)); e = list_next(e)){
                         //skipped an fd number
-                        if(list_entry(list_prev(e), struct fdesc, elem)->fd != (list_entry(e, struct fdesc, elem)->fd - 1)){
+
+                        if(list_entry(e, struct fdesc, elem)->fd != 2 && 
+                            list_entry(list_prev(e), struct fdesc, elem)->fd != (list_entry(e, struct fdesc, elem)->fd - 1)){
                             filed->fd = list_entry(e, struct fdesc, elem)->fd - 1;
                             list_insert(e, &(filed->elem));
                             (f->eax) = list_entry(e, struct fdesc, elem)->fd - 1;
+                            break;
                         }
                         //got through to last element in list
-                        if(list_entry(e, struct fdesc, elem)->fd == (list_size(&(current->fdt)) + 1)){
+                        else if(list_entry(e, struct fdesc, elem)->fd == (list_size(&(current->fdt)) + 1)){
                             filed->fd = list_size(&(current->fdt)) + 2;
                             list_push_back(&(current->fdt), &(filed->elem));
                             (f->eax) = list_size(&(current->fdt)) + 2;
+                            break;
                         }
 
 
@@ -223,16 +232,18 @@ syscall_handler (struct intr_frame *f UNUSED)
         case SYS_FILESIZE:
             sema_down(&sema);
             struct fdesc *fl;
-
-            if((fl = findFD(&(current->fdt), *(call + 1))) == NULL){
-                printf("uh oh");
+            fl = findFD(&(current->fdt), *(call + 1));
+            if(fl == NULL){
+                printf("%s: exit(%d)\n", name, test);
+                sema_up(&sema);
+                thread_exit();
                 break;
             }
             (f->eax) = file_length(fl->f);
             sema_up(&sema);
             break;
         case SYS_READ:
-            if(!check_string((uint8_t *)(call + 2))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string2((uint8_t *)*(call + 2), (uint8_t *)*(call + 3))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
             sema_down(&sema);
             if(*(call + 1) == 0){
                 int i;
@@ -242,15 +253,18 @@ syscall_handler (struct intr_frame *f UNUSED)
                 (f->eax) = *(call + 3);
                 break;
             }
-            if(fl = findFD(&(current->fdt), *(call + 1)) == NULL){
-                printf("uh oh");
+            fl = findFD(&(current->fdt), *(call + 1));
+            if(fl == NULL){
+                printf("%s: exit(%d)\n", name, test);
+                sema_up(&sema);
+                thread_exit();
                 break;
             }
             (f->eax) = file_read(fl->f, *(call + 2), *(call + 3));
             sema_up(&sema);
             break;
         case SYS_WRITE:
-            if(!check_string2((uint8_t *)(call + 2), (uint8_t *)*(call + 3))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
+            if(!check_string2((uint8_t *)*(call + 2), (uint8_t *)*(call + 3))) {  printf("%s: exit(%d)\n", name, test);thread_exit(); }
             sema_down(&sema);
             if(*(call + 1) == 1){
                 int i;
@@ -261,8 +275,11 @@ syscall_handler (struct intr_frame *f UNUSED)
                 (f->eax) = *(call + 3);
             }
             else{
-                if(fl = findFD(&(current->fdt), *(call + 1)) == NULL){
-                    printf("uh oh");
+                fl = findFD(&(current->fdt), *(call + 1));
+                if(fl == NULL){
+                    printf("%s: exit(%d)\n", name, test);
+                    sema_up(&sema);
+                    thread_exit();
                     break;
                 }
                 (f->eax) = file_write(fl->f, *(call + 2), *(call + 3));
@@ -271,8 +288,11 @@ syscall_handler (struct intr_frame *f UNUSED)
             break;
         case SYS_SEEK:
             sema_down(&sema);
-            if(fl = findFD(&(current->fdt), *(call + 1)) == NULL){
-                printf("uh oh");
+            fl = findFD(&(current->fdt), *(call + 1));
+            if(fl == NULL){
+                printf("%s: exit(%d)\n", name, test);
+                sema_up(&sema);
+                thread_exit();
                 break;
             }
             file_seek(&fl->f, (call + 2));
@@ -280,8 +300,11 @@ syscall_handler (struct intr_frame *f UNUSED)
 
         case SYS_TELL:
             sema_down(&sema);
-            if(fl = findFD(&(current->fdt), *(call + 1)) == NULL){
-                printf("uh oh");
+            fl = findFD(&(current->fdt), *(call + 1));
+            if(fl == NULL){
+                printf("%s: exit(%d)\n", name, test);
+                sema_up(&sema);
+                thread_exit();
                 break;
             }
             (f->eax) = file_tell(fl->f);
@@ -290,10 +313,15 @@ syscall_handler (struct intr_frame *f UNUSED)
             break;
         case SYS_CLOSE:
             sema_down(&sema);
-            if(fl = findFD(&(current->fdt), *(call + 1)) == NULL){
-                printf("uh oh");
+            fl = findFD(&(current->fdt), *(call + 1));
+            if(fl == NULL){
+ //               printf("uh oh");
+                printf("%s: exit(%d)\n", name, test);
+                sema_up(&sema);
+                thread_exit();
                 break;
             }
+            ftemp = fl->f;
             file_close(fl->f);
             list_remove(&fl->elem);
             free(fl);
